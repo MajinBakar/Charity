49caef38f27a174d444e47efc3758e728ff231fd
MajinBakar <madani.aboubekr@gmail.com>
2022-08-25 11:03:16 +0200

    charity updates

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Assalumu' Alaykoum, Bienvenue sur Charity
+         Assalumu Alaykoum, Bienvenue sur Charity
         </h1>
 
         <p className={styles.description}>
-          Assalumu' Alaykoum, Bienvenue sur Charity
+          Assalumu Alaykoum, Bienvenue sur Charity
         </p>
       </main>
 
